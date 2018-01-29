a9472224127747e2ddcd45e9ca0d0c8ea069b857
Kamila <kamila.januaria@gmail.com>
2018-01-29 10:09:52 -0200

    Consertando BuscaSP

--- meu-politico/src/main/java/com/meupolitico/controller/ProcessosRestController.java
+++ meu-politico/src/main/java/com/meupolitico/controller/ProcessosRestController.java
@@ -28,38 +28,44 @@ public class ProcessosRestController {
 	@RequestMapping(value = "/BuscaMG", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
 	private void buscaProcessosMinasGerais(String nomePolitico) throws MalformedURLException, IOException {
 
-		// final String link =
-		// "http://www4.tjmg.jus.br/juridico/sf/proc_resultado_nome.jsp?tipoPesquisa=2&txtProcesso=&comrCodigo=24"
-		// + "&nomePessoa=" + "Fernando%20Damata%20Pimentel"
-		// +
-		// "&tipoPessoa=X&naturezaProcesso=0&situacaoParte=X&codigoOAB=&tipoOAB=N&ufOAB=MG&numero=20&select=1&tipoConsulta=1&natureza=0&ativoBaixado=X";
+		nomePolitico = "Alberto+Goldman";
+		final String linkTJ_SP = "https://esaj.tjsp.jus.br/cpopg/search.do?conversationId=&dadosConsulta.localPesquisa.cdLocal=-1&cbPesquisa=NMPARTE&dadosConsulta.tipoNuProcesso=UNIFICADO&dadosConsulta.valorConsulta="
+				+ nomePolitico + "&uuidCaptcha=";
 
-		final String link = "http://www4.tjmg.jus.br/juridico/sf/proc_resultado_nome.jsp?tipoPesquisa=2&txtProcesso=&comrCodigo="
-				+ 24 + "&nomePessoa=" + nome
-				+ "&tipoPessoa=X&naturezaProcesso=0&situacaoParte=X&codigoOAB=&tipoOAB=N&ufOAB=MG&numero=1&select=1&tipoConsulta=1&natureza=0&ativoBaixado=X";
+		Document html = Jsoup.connect(linkTJ_SP).validateTLSCertificates(false).post();
 
-		final String linkTJM_SP = "https://esaj.tjsp.jus.br/cpopg/search.do?conversationId=&dadosConsulta.localPesquisa.cdLocal=-1&cbPesquisa=NMPARTE&dadosConsulta.tipoNuProcesso=UNIFICADO&dadosConsulta.valorConsulta=Alberto+Goldman&uuidCaptcha=";
-		try {
-			Jsoup.connect(link).timeout(10000).validateTLSCertificates(false).get();
-		} catch (Exception e) {
-			e.getMessage();
+		ArrayList<String> linkProcessos = new ArrayList<String>();
+
+		Elements elements = html.select("div#listagemDeProcessos");
+
+		Elements elementsProcessos = elements.get(0).getElementsByClass("nuProcesso");
+
+		for (int i = 1; i < elementsProcessos.size(); i++) {
+			linkProcessos.add("https://esaj.tjsp.jus.br" + elementsProcessos.get(i).getAllElements().attr("href"));
 		}
 
-		// SAO PAULO
-		Document html = Jsoup.parse(new URL(link).openStream(), "ISO-8859-9", linkTJM_SP);
-		Elements form = html.select("table.tabela_formulario");
+		Politico politico = new Politico();
+		politico.setNome(nomePolitico);
 
-		ArrayList<String> numerosChaveProcessos = new ArrayList<>();
+		ArrayList<ProcessoJudicial> listaDeProcessos = new ArrayList<ProcessoJudicial>();
 
-		for (Element element : form.get(0).getAllElements()) {
-			numerosChaveProcessos.add(element.getAllElements().get(3).text().substring(2));
+		for (String processos : linkProcessos) {
+
+			Document html_processo = Jsoup.connect(processos).validateTLSCertificates(false).post();
+			Element detalhesProcesso = html_processo.getElementsByClass("secaoFormBody").get(1);
+			ProcessoJudicial processoJudicial = new ProcessoJudicial();
+			processoJudicial.setDescricao(detalhesProcesso.getElementsByClass("secaoFormBody").get(0)
+					.getElementsByTag("tr").get(6).getElementsByTag("td").get(1).text());
+			processoJudicial.setNumero(detalhesProcesso.getElementsByClass("secaoFormBody").get(0)
+					.getElementsByTag("tr").get(1).getElementsByTag("td").get(0).text());
+			processoJudicial.setPolitico(politico);
+			listaDeProcessos.add(processoJudicial);
+
 		}
 
-		final String allProcessos = "http://www4.tjmg.jus.br/juridico/sf/proc_resultado2.jsp?pessCodigo="
-				+ numerosChaveProcessos.get(0) + "&situacaoParte=X&naturezaProcesso=0" + "&comrCodigo=" + 24
-				+ "&numero=" + 20;
+		politico.setProcessosJudiciais(listaDeProcessos);
 
-		Document html2 = Jsoup.parse(new URL(allProcessos).openStream(), "ISO-8859-9", allProcessos);
+		return politico;
 
 	}
 
